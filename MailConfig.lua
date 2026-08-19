e65e38cd0ed99b2cd2a4950ee2d00451a81a686a
dracular111 <tranhuuthe179@gmail.com>
2026-08-19 20:48:11 +0700

    Update MailConfig.lua

--- MailConfig.lua
+++ MailConfig.lua
@@ -17,91 +17,90 @@ return {
     ITEMS = {
         --["All Huges"] = {Class = "Pet", Id = "All Huges", MinAmount = 1},
 
-        ["Send Diamonds"]                     = {Class = "Currency", Id = "Diamonds",          KeepAmount = "5m", MinAmount = "50m"},
-        ["Gargantuan Machine Hippomelon Egg"] = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Space Egg"]               = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
+        ["Send Diamonds"]                     = {Class = "Currency", Id = "Diamonds", KeepAmount = "5m", MinAmount = "50m"},
+        ["Gargantuan Machine Hippomelon Egg"] = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Space Egg"]               = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
 
-        ["Gargantuan Machine Egg 1"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Gargantuan Machine Egg 2"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Gargantuan Machine Egg 3"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Gargantuan Machine Egg 4"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Gargantuan Machine Egg 5"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Gargantuan Machine Egg 6"]          = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
+        ["Gargantuan Machine Egg 1"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Gargantuan Machine Egg 2"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Gargantuan Machine Egg 3"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Gargantuan Machine Egg 4"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Gargantuan Machine Egg 5"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Gargantuan Machine Egg 6"]          = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
 
-        ["Exclusive Egg 46"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 47"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 48"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 49"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 50"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 51"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 52"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 53"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 54"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
+        ["Exclusive Egg 46"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 47"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 48"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 49"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 50"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 51"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 52"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 53"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 54"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 55"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 56"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 57"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 58"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 59"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 60"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 61"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 62"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 63"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 64"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 65"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Exclusive Egg 66"]                   = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
 
-        ["Exclusive Egg 55"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 56"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 57"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 58"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 59"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 60"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 61"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 62"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 63"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 64"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 65"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Exclusive Egg 66"]                  = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
+        ["Huge Machine Egg 1"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Huge Machine Egg 2"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Huge Machine Egg 3"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Huge Machine Egg 4"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Huge Machine Egg 5"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
+        ["Huge Machine Egg 6"]                 = {Class = "Egg", Id = "Egg", MinAmount = 1, KeepAmount = 0},
 
-        ["Huge Machine Egg 1"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Huge Machine Egg 2"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Huge Machine Egg 3"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Huge Machine Egg 4"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Huge Machine Egg 5"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
-        ["Huge Machine Egg 6"]                = {Class = "Egg",      Id = "Egg",               MinAmount = 1,    KeepAmount = 0},
+        ["Hype Egg"]                           = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1, KeepAmount = 0},
+        ["Hype Egg 2"]                         = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1, KeepAmount = 0},
+        ["Hype Egg 3"]                         = {Class = "Lootbox", Id = "Hype Egg 3", MinAmount = 1, KeepAmount = 0},
 
-        ["Hype Egg"]                          = {Class = "Lootbox",  Id = "Hype Egg",           MinAmount = 1,    KeepAmount = 0},
-        ["Hype Egg 2"]                        = {Class = "Lootbox",  Id = "Hype Egg 2",         MinAmount = 1,    KeepAmount = 0},
-        ["Hype Egg 3"]                        = {Class = "Lootbox",  Id = "Hype Egg 3",         MinAmount = 1,    KeepAmount = 0},
+        ["MVP Key"]                            = {Class = "Misc", Id = "MVP Key", MinAmount = 50, KeepAmount = 0},
+        ["MVP Key Upper Half"]                 = {Class = "Misc", Id = "MVP Key Upper Half", MinAmount = 100, KeepAmount = 0},
+        ["MVP Key Lower Half"]                 = {Class = "Misc", Id = "MVP Key Lower Half", MinAmount = 200, KeepAmount = 0},
+        ["Time Trial Gift"]                    = {Class = "Lootbox", Id = "Time Trial Gift", MinAmount = 50, KeepAmount = 0},
+        ["Time Trial Gift 2"]                  = {Class = "Lootbox", Id = "Time Trial Gift 2", MinAmount = 50, KeepAmount = 0},
 
-        ["MVP Key"]                           = {Class = "Misc",     Id = "MVP Key",            MinAmount = 50,   KeepAmount = 0},
-        ["MVP Key Upper Half"]                = {Class = "Misc",     Id = "MVP Key Upper Half", MinAmount = 100,  KeepAmount = 0},
-        ["MVP Key Lower Half"]                = {Class = "Misc",     Id = "MVP Key Lower Half", MinAmount = 200,  KeepAmount = 0},
-        ["Time Trial Gift"]                   = {Class = "Lootbox",  Id = "Time Trial Gift",    MinAmount = 50,   KeepAmount = 0},
-        ["Time Trial Gift 2"]                 = {Class = "Lootbox",  Id = "Time Trial Gift 2",  MinAmount = 50,   KeepAmount = 0},
+        ["Mega Potion Chest"]                  = {Class = "Lootbox", Id = "Mega Potion Chest", MinAmount = 1, KeepAmount = 0},
+        ["Mega Enchant Chest"]                 = {Class = "Lootbox", Id = "Mega Enchant Chest", MinAmount = 1, KeepAmount = 0},
 
-        ["Mega Potion Chest"]                 = {Class = "Lootbox",  Id = "Mega Potion Chest",  MinAmount = 1,    KeepAmount = 0},
-        ["Mega Enchant Chest"]                = {Class = "Lootbox",  Id = "Mega Enchant Chest", MinAmount = 1,    KeepAmount = 0},
+        ["Rainbow Mini Chest"]                 = {Class = "Misc", Id = "Rainbow Mini Chest", MinAmount = 20, KeepAmount = 0},
 
-        ["Rainbow Mini Chest"]                = {Class = "Misc",     Id = "Rainbow Mini Chest", MinAmount = 20,   KeepAmount = 0},
+        ["Charm Stone"]                        = {Class = "Misc", Id = "Charm Stone", MinAmount = 200, KeepAmount = 0}, --price 29k
+        ["Charm Chisel"]                       = {Class = "Misc", Id = "Charm Chisel", MinAmount = 25, KeepAmount = 0},  --price 227k
+        ["Charm Hammer"]                       = {Class = "Misc", Id = "Charm Stone", MinAmount = 400, KeepAmount = 0}, --price 12k
 
-        ["Charm Stone"]                       = {Class = "Misc",     Id = "Charm Stone",        MinAmount = 200,  KeepAmount = 0},    --price 29k
-        ["Charm Chisel"]                      = {Class = "Misc",     Id = "Charm Chisel",       MinAmount = 25,   KeepAmount = 0},    --price 227k
-        ["Charm Hammer"]                      = {Class = "Misc",     Id = "Charm Stone",        MinAmount = 400,  KeepAmount = 0},    --price 12k
+        ["Treasure Charm"]                     = {Class = "Charm", Id = "Treasure", MinAmount = 1, KeepAmount = 0}, --price 3b4
+        ["Overload Charm"]                     = {Class = "Charm", Id = "Overload", MinAmount = 2, KeepAmount = 0},  --price 3m
+        ["Royalty Charm"]                      = {Class = "Charm", Id = "Royalty", MinAmount = 1, KeepAmount = 0},   --price 9m
+        --["Lightning Charm"]                  = {Class = "Charm", Id = "Lightning", MinAmount = 20, KeepAmount = 0}, --price 11k
+        --["Glittering Charm"]                 = {Class = "Charm", Id = "Glittering", MinAmount = 1, KeepAmount = 0}, --price 43k
 
-        ["Treasure Charm"]                    = {Class = "Charm",    Id = "Treasure",            MinAmount = 1,    KeepAmount = 0},    --price 3b4
-        ["Overload Charm"]                    = {Class = "Charm",    Id = "Overload",             MinAmount = 2,    KeepAmount = 0},    --price 3m
-        ["Royalty Charm"]                     = {Class = "Charm",    Id = "Royalty",              MinAmount = 1,    KeepAmount = 0},    --price 9m
-        --["Lightning Charm"]                = {Class = "Charm",    Id = "Lightning",            MinAmount = 20,   KeepAmount = 0},    --price 11k
-        --["Glittering Charm"]               = {Class = "Charm",    Id = "Glittering",           MinAmount = 1,    KeepAmount = 0},    --price 43k
+        ["Agility Charm"]                      = {Class = "Charm", Id = "Agility", MinAmount = 800, KeepAmount = 0},   --price 6k
+        ["Strength Charm"]                     = {Class = "Charm", Id = "Strength", MinAmount = 1000, KeepAmount = 0}, --price 5k
+        ["Coins Charm"]                        = {Class = "Charm", Id = "Coins", MinAmount = 1700, KeepAmount = 0},    --price 3k
+        ["Criticals Charm"]                    = {Class = "Charm", Id = "Criticals", MinAmount = 1700, KeepAmount = 0}, --price 3k
+        ["TNT Charm"]                          = {Class = "Charm", Id = "TNT", MinAmount = 500, KeepAmount = 0},       --price 10k
+        ["Diamonds Charm"]                     = {Class = "Charm", Id = "Diamonds", MinAmount = 600, KeepAmount = 0},  --price 8k
+        ["XP Charm"]                           = {Class = "Charm", Id = "XP", MinAmount = 25, KeepAmount = 0},          --price 193k
 
-        ["Agility Charm"]                     = {Class = "Charm",    Id = "Agility",              MinAmount = 800,  KeepAmount = 0},    --price 6k
-        ["Strength Charm"]                    = {Class = "Charm",    Id = "Strength",             MinAmount = 1000, KeepAmount = 0},    --price 5k
-        ["Coins Charm"]                       = {Class = "Charm",    Id = "Coins",                MinAmount = 1700, KeepAmount = 0},    --price 3k
-        ["Criticals Charm"]                   = {Class = "Charm",    Id = "Criticals",            MinAmount = 1700, KeepAmount = 0},    --price 3k
-        ["TNT Charm"]                         = {Class = "Charm",    Id = "TNT",                  MinAmount = 500,  KeepAmount = 0},    --price 10k
-        ["Diamonds Charm"]                    = {Class = "Charm",    Id = "Diamonds",             MinAmount = 600,  KeepAmount = 0},    --price 8k
-        ["XP Charm"]                          = {Class = "Charm",    Id = "XP",                   MinAmount = 25,   KeepAmount = 0},    --price 193k
+        ["Magic Shard"]                        = {Class = "Misc", Id = "Magic Shard", MinAmount = 50, KeepAmount = 0},    --price 77k
+        ["Insta Plant Capsule"]                = {Class = "Misc", Id = "Insta Plant Capsule", MinAmount = 200, KeepAmount = 0}, --24k
 
-        ["Magic Shard"]                       = {Class = "Misc",     Id = "Magic Shard",          MinAmount = 50,   KeepAmount = 0},
-        ["Insta Plant Capsule"]               = {Class = "Misc",     Id = "Insta Plant Capsule",  MinAmount = 200,  KeepAmount = 0},    --24k
-
-        ["Crystal Key"]                       = {Class = "Misc",     Id = "Crystal Key",          MinAmount = 600,  KeepAmount = 0},    --price 8.0k
-        ["Secret Key"]                        = {Class = "Misc",     Id = "Secret Key",           MinAmount = 400,  KeepAmount = 0},    --price 11.8k
-        ["Fantasy Key"]                       = {Class = "Misc",     Id = "Fantasy Key",           MinAmount = 150,  KeepAmount = 0},    --price 35.8k
-        ["Tech Key"]                          = {Class = "Misc",     Id = "Tech Key",              MinAmount = 250,  KeepAmount = 0},    --price 20k
-        ["Void Key"]                          = {Class = "Misc",     Id = "Void Key",              MinAmount = 250,  KeepAmount = 0},    --price 21.5k
-        ["Prison Key"]                        = {Class = "Misc",     Id = "Prison Key",             MinAmount = 50,   KeepAmount = 0},    --price 128k
-        ["Hacker Key"]                        = {Class = "Misc",     Id = "Hacker Key",             MinAmount = 50,   KeepAmount = 0},    --price 110k
-        ["Treasure Hideout Key"]              = {Class = "Misc",     Id = "Treasure Hideout Key",  MinAmount = 550,  KeepAmount = 0},    --price 9.1k
-        ["Golden Prison Key"]                 = {Class = "Misc",     Id = "Golden Prison Key",    MinAmount = 25,   KeepAmount = 0},    --price 197k
+        ["Crystal Key"]                        = {Class = "Misc", Id = "Crystal Key", MinAmount = 600, KeepAmount = 0},       --price 8.0k
+        ["Secret Key"]                         = {Class = "Misc", Id = "Secret Key", MinAmount = 400, KeepAmount = 0},        --price 11.8k
+        ["Fantasy Key"]                        = {Class = "Misc", Id = "Fantasy Key", MinAmount = 150, KeepAmount = 0},       --price 35.8k
+        ["Tech Key"]                           = {Class = "Misc", Id = "Tech Key", MinAmount = 250, KeepAmount = 0},          --price 20k
+        ["Void Key"]                           = {Class = "Misc", Id = "Void Key", MinAmount = 250, KeepAmount = 0},          --price 21.5k
+        ["Prison Key"]                         = {Class = "Misc", Id = "Prison Key", MinAmount = 50, KeepAmount = 0},          --price 128k
+        ["Hacker Key"]                         = {Class = "Misc", Id = "Hacker Key", MinAmount = 50, KeepAmount = 0},          --price 110k
+        ["Treasure Hideout Key"]               = {Class = "Misc", Id = "Treasure Hideout Key", MinAmount = 550, KeepAmount = 0}, --price 9.1k
+        ["Golden Prison Key"]                  = {Class = "Misc", Id = "Golden Prison Key", MinAmount = 25, KeepAmount = 0},   --price 197k
     }
 }
